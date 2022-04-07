fcaafee302c28dc193af3fe00ad4bcf429f555b0
Zubair-Eb <zubair.yousaf-eberl@gmx.de>
2022-04-07 15:14:25 +0200

    Update-07.04.22

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,34 @@ public class App {
     
     public static void main(String[] args) {
      
+
+        ausgabe("Ergebnis+: "+ addieren(8, 9));
+        ausgabe("Ergebnis-: "+ sub(8, 2));
+        ausgabe("Ergebnis*: "+ multi(8, 9));
+        ausgabe("Ergebnis/: "+ division(8, 0));
     }
 
     public static void ausgabe(String outputStr) {
         System.out.println(outputStr);
     }
+
+    public static Double addieren(double a,double b) {
+        return a + b;
+    }
+    public static Double sub(double a,double b) {
+        return a - b;
+    }
+    public static Double multi(double a,double b) {
+        return a * b;
+    }
+    public static String division(double a,double b) {
+        if (b !=0) {
+            return Double.toString(a/b);
+        } else {
+            return "\"Error!\"";
+        }
+        
+    }        
 
 
 
