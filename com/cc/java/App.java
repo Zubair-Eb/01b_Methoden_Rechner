7e0776851de85821a66e07f2ed97f72ce65d262f
Zubair-Eb <zubair.yousaf-eberl@gmx.de>
2022-04-08 14:51:00 +0200

    Rechner

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,44 @@ public class App {
     
     public static void main(String[] args) {
      
-
+        ausgabe("---------Test-----------");
         ausgabe("Ergebnis+: "+ addieren(8, 9));
         ausgabe("Ergebnis-: "+ sub(8, 2));
         ausgabe("Ergebnis*: "+ multi(8, 9));
         ausgabe("Ergebnis/: "+ division(8, 0));
+    
+        ausgabe("---------Rechner-----------");
+
+        ausgabe(rechner(4, 5, "+"));
+        ausgabe(rechner(4, 5, "-"));
+        ausgabe(rechner(4, 5, "*"));
+        ausgabe(rechner(4, 5, ":"));
+        ausgabe(rechner(4, 0, ":"));
+        ausgabe(rechner(4, 5, "#!?_"));
+
+
+
     }
 
+           
+
+    public static String rechner(double a, double b, String op) {
+
+        switch (op) {
+            case "+":
+                return "Ergebnis: "+ addieren(a, b);
+             case "-":
+                return "Ergebnis: "+ sub(a, b);
+            case "*":
+                return "Ergebnis: "+ multi(a, b);
+            case ":":
+                return "Ergebnis: "+ division(a, b);
+            default:
+                return "Irgendwas ging schief!";
+        }
+
+
+    }
     public static void ausgabe(String outputStr) {
         System.out.println(outputStr);
     }
